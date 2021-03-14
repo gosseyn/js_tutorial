a19d4abc34ce68e6ec4e65a461cf943d55ba68d6
gosseyn <gosseynx@gmail.com>
2021-03-14 17:58:01 +0100

    Finished the palindrome Tester

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 let Phrase = require("gosseyn-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing");
+function palindromeTester(event) {
+    event.preventDefault();
+    
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
-let phrase = new Phrase(string);
-
-if(phrase.palindrome()){
-    alert(`"${phrase.content}" is a palindrome.`);
-} else {
-    alert(`"${phrase.content}" is not a palindrome.`);
+    if(phrase.palindrome()){
+	palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome.`;
+    } else {
+        palindromeResult.innerHTML = `"<strong>"${phrase.content}"</strong> is not a palindrome.`;
+    }
 }
+document.addEventListener("DOMContentLoaded",function(){
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event){
+	palindromeTester(event);
+    });
+});
+
+
+
+
+
 
